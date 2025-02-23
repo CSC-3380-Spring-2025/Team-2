f9d3dc982888da86ada2f0b69bea372e3abf3f01
Melanie (Jnea) Steiner <118273237+an-npc@users.noreply.github.com>
2025-02-23 17:43:44 -0600

    test2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Link } from 'expo-router';
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen!!!</Text>
+      <Text style={styles.text}>Home Screen!!</Text>
     </View>
   );
 }
