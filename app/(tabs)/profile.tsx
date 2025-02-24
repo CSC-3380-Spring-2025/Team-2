7d988d43bf9dc850bf231b1afe03b837a35ea39d
Melanie (Jnea) Steiner <118273237+an-npc@users.noreply.github.com>
2025-02-23 18:11:53 -0600

    Creatinng profile tsx file and starter code

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button, Dimensions, Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function Profile() {
+    return (
+        <View>
+            <Text>Profile Screen Here!!!</Text>
+        </View>
+    );
+}
