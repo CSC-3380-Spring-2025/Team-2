85c428798999a186889cb22161268fe5df38564d
Jack O'Connor <jackoconnor219@hotmail.com>
2025-03-22 02:39:59 -0500

    new file for saving globally used StyleSheets

---
+++ assets/styles/globals.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20
+    },
+    headerText: {
+        fontFamily: 'poppins',
+        fontSize: 20,
+        color: '#000000'
+    },
+    headerText2: {
+        fontFamily: 'poppins',
+        fontSize: 18,
+        color: '#000000'
+    },
+})
